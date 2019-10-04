99a4320b1d68a358cd04f600b4348df0d41a0adc
michalbiesek <michal.biesek@intel.com>
2019-10-04 10:58:58 +0200

    MODIFY EXAMPLE

--- examples/hello_hbw_example.c
+++ examples/hello_hbw_example.c
@@ -22,7 +22,7 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <hbwmalloc.h>
+#include <memkind.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -30,52 +30,18 @@
 
 int main(int argc, char **argv)
 {
-    const size_t size = 512;
-    char *default_str = NULL;
-    char *hbw_str = NULL;
-    char *hbw_hugetlb_str = NULL;
-    int err = 0;
+    const size_t size = 512 *1024;
+    char *default_str = memkind_malloc(MEMKIND_HBW, size);
 
-    default_str = (char *)malloc(size);
-    if (default_str == NULL) {
-        perror("malloc()");
-        fprintf(stderr, "Unable to allocate default string\n");
-        err = errno ? -errno : 1;
-        goto exit;
-    }
-    hbw_str = (char *)hbw_malloc(size);
-    if (hbw_str == NULL) {
-        perror("hbw_malloc()");
-        fprintf(stderr, "Unable to allocate hbw string\n");
-        err = errno ? -errno : 1;
-        goto exit;
-    }
-    err = hbw_posix_memalign_psize((void **)&hbw_hugetlb_str, 2097152, size,
-                                   HBW_PAGESIZE_2MB);
-    if (err) {
-        perror("hbw_posix_memalign()");
-        fprintf(stderr, "Unable to allocate hbw hugetlb string\n");
-        err = errno ? -errno : 1;
-        goto exit;
+    while (1) {
+        default_str = (char *)memkind_malloc(MEMKIND_HBW, size);
+        fprintf(stderr, "malloc\n");
+        if (!default_str)
+        {
+           fprintf(stderr, "Unable to allocate default string\n");
+           break;
+        }
     }
 
-    sprintf(default_str, "Hello world from standard memory\n");
-    sprintf(hbw_str, "Hello world from high bandwidth memory\n");
-    sprintf(hbw_hugetlb_str, "Hello world from high bandwidth 2 MB paged memory\n");
-
-    fprintf(stdout, "%s", default_str);
-    fprintf(stdout, "%s", hbw_str);
-    fprintf(stdout, "%s", hbw_hugetlb_str);
-
-exit:
-    if (hbw_hugetlb_str) {
-        hbw_free(hbw_hugetlb_str);
-    }
-    if (hbw_str) {
-        hbw_free(hbw_str);
-    }
-    if (default_str) {
-        free(default_str);
-    }
-    return err;
+    return 0;
 }
