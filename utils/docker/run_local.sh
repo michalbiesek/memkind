5061c3e3e52e11dfc6f9b435d3340a71600a65be
michalbiesek <michal.biesek@intel.com>
2019-11-19 20:08:21 +0100

    ttt

--- utils/docker/run_local.sh
+++ utils/docker/run_local.sh
@@ -32,7 +32,7 @@
 set -e
 
 DOCKER_IMAGE_NAME="$1"
-export PMEM_PATH=${PMEM_PATH:-/tmp/}
+export PMEM_HOST_PATH=${PMEM_HOST_PATH:-/tmp/}
 
 if [[ ! -f "$DOCKER_IMAGE_NAME" ]]; then
     echo "Docker image "$DOCKER_IMAGE_NAME" does not exist."
@@ -61,7 +61,7 @@ docker run --rm \
            --env TEST_SUITE_NAME="$TEST_SUITE_NAME" \
            --env TBB_LIBRARY_VERSION="$TBB_LIBRARY_VERSION" \
            --env NDCTL_LIBRARY_VERSION="$NDCTL_LIBRARY_VERSION" \
-           --env PMEM_CONTAINER_PATH="$PMEM_PATH" \
+           --env PMEM_CONTAINER_PATH="$PMEM_CONTAINER_PATH" \
            --mount type=bind,source="$MEMKIND_HOST_WORKDIR",target="$MEMKIND_CONTAINER_WORKDIR" \
-           --mount type=bind,source="$PMEM_PATH",target="$PMEM_CONTAINER_PATH" \
+           --mount type=bind,source="$PMEM_HOST_PATH",target="$PMEM_CONTAINER_PATH" \
            memkind_cont utils/docker/docker_run_build.sh
