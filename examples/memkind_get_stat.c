8311a94d4b5ad8935807a06d9e733f0fc58dc69b
michalbiesek <michal.biesek@intel.com>
2020-04-24 11:57:15 +0200

    Provide example

--- examples/memkind_get_stat.c
+++ examples/memkind_get_stat.c
@@ -24,46 +24,27 @@
 
 #include <memkind.h>
 #include <stdio.h>
+#include <string.h>
+
+#define ALLOCATION_SIZE 1024
+#define ARRAY_SIZE 5000
+#define OUTER_LOOP_SIZE 5000
 
 int main()
 {
-    const size_t size = 1024;
+    char* test_array[ARRAY_SIZE];
+    
+    for(int j =0; j <OUTER_LOOP_SIZE; j++){
 
-    size_t stats_active;
-    size_t stats_resident;
-    size_t stats_allocated;
-    size_t stats_kind_regular_active;
-    size_t stats_kind_regular_resident;
-    size_t stats_kind_regular_allocated;
-
-    char *ptr_regular = (char *)memkind_malloc(MEMKIND_REGULAR, size);
-
-    fprintf(stdout,
-            "This example shows how to use memkind API to retrieve information about allocation stats.\n");
-
-    snprintf(ptr_regular, size,
-             "Hello world from regular kind memory - ptr_regular.\n");
-
-    memkind_update_cached_stats();
-    memkind_get_stat(NULL, MEMKIND_STAT_TYPE_RESIDENT, &stats_resident);
-    memkind_get_stat(NULL, MEMKIND_STAT_TYPE_ACTIVE, &stats_active);
-    memkind_get_stat(NULL, MEMKIND_STAT_TYPE_ALLOCATED, &stats_allocated);
-    memkind_get_stat(MEMKIND_REGULAR, MEMKIND_STAT_TYPE_RESIDENT,
-                     &stats_kind_regular_resident);
-    memkind_get_stat(MEMKIND_REGULAR, MEMKIND_STAT_TYPE_ACTIVE,
-                     &stats_kind_regular_active);
-    memkind_get_stat(MEMKIND_REGULAR, MEMKIND_STAT_TYPE_ALLOCATED,
-                     &stats_kind_regular_allocated);
-
-    fprintf(stdout,
-            "\n Global stats \nresident %zu \nactive %zu, \nallocated %zu \n",
-            stats_resident, stats_active, stats_allocated);
-    fprintf(stdout,
-            "\n MEMKIND_REGULAR stats \nresident %zu \nactive %zu, \nallocated %zu \n",
-            stats_kind_regular_resident, stats_kind_regular_active,
-            stats_kind_regular_allocated);
-
-    memkind_free(NULL, ptr_regular);
+        for(int i =0; i <ARRAY_SIZE; i++){
+            char *ptr_regular = (char *)memkind_malloc(MEMKIND_REGULAR, ALLOCATION_SIZE);
+            memset(ptr_regular, 0 ,ALLOCATION_SIZE);
+            test_array[i] = ptr_regular;
+        }
+        for(int i =0; i <ARRAY_SIZE; i++){
+            memkind_free(NULL,test_array[i]);
+        }
+    }
 
     return 0;
 }
