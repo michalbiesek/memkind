2fa5ef7773d4a205e141015aa76506e78d2007eb
Jenkins <jenkins@example.com>
2020-09-30 09:34:15 -0400

    test commit

--- autodax/autodax.c
+++ autodax/autodax.c
@@ -10,50 +10,107 @@
 #include <string.h>
 #include <unistd.h>
 
-static memkind_t kind;
+#define AUTODAX_EXPORT __attribute__((visibility("default")))
+#define AUTODAX_INIT __attribute__((constructor))
 
-#define KMEM_DAX_EXPORT __attribute__((visibility("default")))
-#define KMEM_DAX_INIT __attribute__((constructor))
 
-static void KMEM_DAX_INIT kmem_init(void){
-   const char *log_str = getenv("KMEM_PATH");
-   if (log_str)
-       fprintf(stdout, "\n KMEM_PAHT %s", log_str);
-    kind = MEMKIND_DEFAULT;
-    fprintf(stdout, "\n kmem_init");
+// Following is the type of HBW memory that is allocated using memkind.
+// By changing this type, this library can be used to allocate other
+// types of memory types (e.g., MEMKIND_HUGETLB, MEMKIND_GBTLB,
+// MEMKIND_HBW_HUGETLB etc.)
+static memkind_t memkind_kind;
+
+
+// This function is executed at library load time.
+// Initialize HBW arena by making a dummy allocation/free at library load
+// time. Until HBW initialization is complete, we must not call any
+// allocation routines with HBW as kind.
+static void AUTODAX_INIT autohbw_load(void)
+{
+    // First set the default memory type this library allocates. This can
+    // be overridden by env variable
+    // Note: 'memkind_hbw_preferred' will allow falling back to DDR but
+    //       'memkind_hbw will not'
+    // Note: If HBM is not installed on a system, memkind_hbw_preferred call
+    //       would fail. Therefore, we need to check for availability first.
+    if (memkind_check_available(MEMKIND_DAX_KMEM) != 0) {
+        memkind_kind = MEMKIND_DEFAULT;
+    } else {
+        memkind_kind = MEMKIND_DAX_KMEM;
+    }
 }
 
-static void *kmem_malloc(size_t size)
+static void *MemkindMalloc(size_t size)
 {
-    fprintf(stdout, "\n enter malloc size: %zu", size);
-    return memkind_malloc(kind, size);
+    return memkind_malloc(memkind_kind, size);
 }
 
-KMEM_DAX_EXPORT void *malloc(size_t size)
+static void *MemkindCalloc(size_t nmemb, size_t size)
 {
-    return kmem_malloc(size);
+    return memkind_calloc(memkind_kind, nmemb, size);
 }
 
-KMEM_DAX_EXPORT void *calloc(size_t nmemb, size_t size)
+static void *MemkindRealloc(void *ptr, size_t size)
 {
-    fprintf(stdout, "\n enter calloc nmemb: %zu size: %zu", nmemb, size);
-    return memkind_calloc(kind, nmemb, size);
+    return memkind_realloc(memkind_kind, ptr, size);
 }
 
-KMEM_DAX_EXPORT void *realloc(void *ptr, size_t size)
+static int MemkindAlign(void **memptr, size_t alignment, size_t size)
 {
-    fprintf(stdout, "\n enter realloc size: %zu", size);
-    return memkind_realloc(kind, ptr, size);
+    return memkind_posix_memalign(memkind_kind, memptr, alignment, size);
 }
 
-KMEM_DAX_EXPORT int posix_memalign(void **memptr, size_t alignment, size_t size)
+// memkind_free does not need the exact kind, if kind is 0. Then
+// the library can figure out the proper kind itself.
+static void MemkindFree(void *ptr)
 {
-    fprintf(stdout, "\n enter posix_memalign size: %zu", size);
-    return memkind_posix_memalign(kind, memptr, alignment, size);
+    memkind_free(memkind_kind, ptr);
 }
 
-KMEM_DAX_EXPORT void free(void *ptr)
+AUTODAX_EXPORT void *malloc(size_t size)
 {
-    fprintf(stdout, "\n enter free");
-    memkind_free(kind, ptr);
+    return MemkindMalloc(size);
+}
+
+AUTODAX_EXPORT void *calloc(size_t nmemb, size_t size)
+{
+    return MemkindCalloc(nmemb, size);
+}
+
+AUTODAX_EXPORT void *realloc(void *ptr, size_t size)
+{
+    return MemkindRealloc(ptr, size);
+}
+
+AUTODAX_EXPORT int posix_memalign(void **memptr, size_t alignment, size_t size)
+{
+    return MemkindAlign(memptr, alignment, size);
+}
+
+// Warn about deprecated function usage.
+AUTODAX_EXPORT void *valloc(size_t size)
+{
+    void *memptr = 0;
+    size_t boundary = sysconf(_SC_PAGESIZE);
+    int status = MemkindAlign(&memptr, boundary, size);
+    if (status == 0 && memptr != 0)
+        return memptr;
+
+    return 0;
+}
+
+// Warn about deprecated function usage.
+AUTODAX_EXPORT void *memalign(size_t boundary, size_t size)
+{
+    void *memptr = 0;
+    int status = MemkindAlign(&memptr, boundary, size);
+    if (status == 0 && memptr != 0)
+        return memptr;
+
+    return 0;
+}
+
+AUTODAX_EXPORT void free(void *ptr)
+{
+    return MemkindFree(ptr);
 }
