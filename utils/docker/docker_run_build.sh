ee8ddf3ce4b006fc39f3cd526199e3e1402ac4b8
michalbiesek <michal.biesek@intel.com>
2020-12-03 09:45:27 +0100

    Add script to install libvirt

--- utils/docker/docker_run_build.sh
+++ utils/docker/docker_run_build.sh
@@ -10,37 +10,4 @@ set -e
 
 export UTILS_PREFIX=utils/docker
 
-if [ -n "$CODECOV_TOKEN" ]; then
-    GCOV_OPTION="--enable-gcov"
-fi
-
-# if ndctl library version is specified install library
-if [ -n "$NDCTL_LIBRARY_VERSION" ]; then
-    "$UTILS_PREFIX"/docker_install_ndctl.sh
-fi
-
-"$UTILS_PREFIX"/docker_install_hwloc.sh
-
-# building memkind sources and tests
-./autogen.sh
-./configure --prefix=/usr $GCOV_OPTION
-make -j "$(nproc)"
-make -j "$(nproc)" checkprogs
-
-# building RPM package
-if [[ $(cat /etc/os-release) = *"Fedora"* ]]; then
-    make -j "$(nproc)" rpm
-fi
-
-# installing memkind
-sudo make install
-
-# if TBB library version is specified install library and use it
-# as MEMKIND_HEAP_MANAGER
-if [ -n "$TBB_LIBRARY_VERSION" ]; then
-    source "$UTILS_PREFIX"/docker_install_tbb.sh
-    HEAP_MANAGER="TBB"
-fi
-if [ -n "$TEST_SUITE_NAME" ]; then
-    "$UTILS_PREFIX"/docker_run_test.sh "$HEAP_MANAGER"
-fi
+"$UTILS_PREFIX"/docker_install_libvirt.sh
