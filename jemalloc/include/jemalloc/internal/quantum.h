1e74eee93d0ef44a2b04f6287df6063452c31e9e
michalbiesek <michal.biesek@intel.com>
2021-02-15 12:39:31 +0100

    Update jemalloc to commit a11be50

--- jemalloc/include/jemalloc/internal/quantum.h
+++ jemalloc/include/jemalloc/internal/quantum.h
@@ -34,7 +34,11 @@
 #    define LG_QUANTUM		3
 #  endif
 #  ifdef __mips__
-#    define LG_QUANTUM		3
+#    if defined(__mips_n32) || defined(__mips_n64)
+#      define LG_QUANTUM		4
+#    else
+#      define LG_QUANTUM		3
+#    endif
 #  endif
 #  ifdef __nios2__
 #    define LG_QUANTUM		3
@@ -60,6 +64,9 @@
 #  endif
 #  ifdef __le32__
 #    define LG_QUANTUM		4
+#  endif
+#  ifdef __arc__
+#    define LG_QUANTUM		3
 #  endif
 #  ifndef LG_QUANTUM
 #    error "Unknown minimum alignment for architecture; specify via "
