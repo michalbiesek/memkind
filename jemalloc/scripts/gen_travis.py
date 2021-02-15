571c08473f4211d94e48b01820dd401b399986d6
michalbiesek <michal.biesek@intel.com>
2021-02-15 12:39:31 +0100

    Update jemalloc to commit 6630c59

--- jemalloc/scripts/gen_travis.py
+++ jemalloc/scripts/gen_travis.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from itertools import combinations
 
 travis_template = """\
 language: generic
-dist: precise
+dist: focal
 
 matrix:
   include:
@@ -30,11 +30,13 @@ script:
 # travis though, we don't test all 2**7 = 128 possible combinations of these;
 # instead, we only test combinations of up to 2 'unusual' settings, under the
 # hope that bugs involving interactions of such settings are rare.
-# Things at once, for C(7, 0) + C(7, 1) + C(7, 2) = 29
 MAX_UNUSUAL_OPTIONS = 2
 
 os_default = 'linux'
 os_unusual = 'osx'
+
+arch_default = 'amd64'
+arch_unusual = 'ppc64le'
 
 compilers_default = 'CC=gcc CXX=g++'
 compilers_unusual = 'CC=clang CXX=clang++'
@@ -47,6 +49,7 @@ configure_flag_unusuals = [
     '--disable-stats',
     '--disable-libdl',
     '--enable-opt-safety-checks',
+    '--with-lg-page=16',
 ]
 
 malloc_conf_unusuals = [
@@ -57,22 +60,24 @@ malloc_conf_unusuals = [
 ]
 
 all_unusuals = (
-    [os_unusual] + [compilers_unusual] + compiler_flag_unusuals
+    [os_unusual] + [arch_unusual] + [compilers_unusual] + compiler_flag_unusuals
     + configure_flag_unusuals + malloc_conf_unusuals
 )
 
 unusual_combinations_to_test = []
-for i in xrange(MAX_UNUSUAL_OPTIONS + 1):
+for i in range(MAX_UNUSUAL_OPTIONS + 1):
     unusual_combinations_to_test += combinations(all_unusuals, i)
 
 gcc_multilib_set = False
+gcc_ppc_set = False
 # Formats a job from a combination of flags
 def format_job(combination):
     global gcc_multilib_set
+    global gcc_ppc_set
 
     os = os_unusual if os_unusual in combination else os_default
     compilers = compilers_unusual if compilers_unusual in combination else compilers_default
-
+    arch = arch_unusual if arch_unusual in combination else arch_default
     compiler_flags = [x for x in combination if x in compiler_flag_unusuals]
     configure_flags = [x for x in combination if x in configure_flag_unusuals]
     malloc_conf = [x for x in combination if x in malloc_conf_unusuals]
@@ -82,6 +87,9 @@ def format_job(combination):
       'percpu_arena:percpu' in malloc_conf or 'background_thread:true' \
       in malloc_conf):
         return ""
+    # gcc is just a redirect to clang on OS X. No need to test both.
+    if os == 'osx' and compilers_unusual in combination:
+        return ""
     if len(malloc_conf) > 0:
         configure_flags.append('--with-malloc-conf=' + ",".join(malloc_conf))
 
@@ -89,14 +97,18 @@ def format_job(combination):
     if os == 'osx' and '--enable-prof' in configure_flags:
         return ""
 
-    # We get some spurious errors when -Warray-bounds is enabled.
-    env_string = ('{} COMPILER_FLAGS="{}" CONFIGURE_FLAGS="{}" '
-	'EXTRA_CFLAGS="-Werror -Wno-array-bounds"').format(
-        compilers, " ".join(compiler_flags), " ".join(configure_flags))
+    # Filter out unsupported OSX configuration on PPC64LE
+    if arch == 'ppc64le' and (
+        os == 'osx'
+        or '-m32' in combination
+        or compilers_unusual in combination
+        ):
+        return ""
 
     job = ""
     job += '    - os: %s\n' % os
-    job += '      env: %s\n' % env_string
+    job += '      arch: %s\n' % arch
+
     if '-m32' in combination and os == 'linux':
         job += '      addons:'
         if gcc_multilib_set:
@@ -106,7 +118,26 @@ def format_job(combination):
             job += '        apt:\n'
             job += '          packages:\n'
             job += '            - gcc-multilib\n'
+            job += '            - g++-multilib\n'
             gcc_multilib_set = True
+
+    # We get some spurious errors when -Warray-bounds is enabled.
+    extra_cflags = ['-Werror', '-Wno-array-bounds']
+    if 'clang' in compilers or os == 'osx':
+        extra_cflags += [
+	    '-Wno-unknown-warning-option',
+	    '-Wno-ignored-attributes'
+	]
+    if os == 'osx':
+        extra_cflags += [
+	    '-Wno-deprecated-declarations',
+	]
+    env_string = ('{} COMPILER_FLAGS="{}" CONFIGURE_FLAGS="{}" '
+        'EXTRA_CFLAGS="{}"'.format(
+        compilers, ' '.join(compiler_flags), ' '.join(configure_flags),
+        ' '.join(extra_cflags)))
+
+    job += '      env: %s\n' % env_string
     return job
 
 include_rows = ""
@@ -127,16 +158,19 @@ include_rows += '''\
       env: CC=gcc CXX=g++ COMPILER_FLAGS="" CONFIGURE_FLAGS="--enable-debug --enable-experimental-smallocx --enable-stats --enable-prof" EXTRA_CFLAGS="-Werror -Wno-array-bounds"
 '''
 
+# Does not seem to be working on newer travis machines. Valgrind has long been a
+# pain point; abandon it for now.
 # Valgrind build bots
-include_rows += '''
-    # Valgrind
-    - os: linux
-      env: CC=gcc CXX=g++ COMPILER_FLAGS="" CONFIGURE_FLAGS="" EXTRA_CFLAGS="-Werror -Wno-array-bounds" JEMALLOC_TEST_PREFIX="valgrind"
-      addons:
-        apt:
-          packages:
-            - valgrind
-'''
+#include_rows += '''
+#    # Valgrind
+#    - os: linux
+#      arch: amd64
+#      env: CC=gcc CXX=g++ COMPILER_FLAGS="" CONFIGURE_FLAGS="" EXTRA_CFLAGS="-Werror -Wno-array-bounds" JEMALLOC_TEST_PREFIX="valgrind"
+#      addons:
+#        apt:
+#          packages:
+#            - valgrind
+#'''
 
 # To enable valgrind on macosx add:
 #
@@ -146,4 +180,4 @@ include_rows += '''
 #
 # It currently fails due to: https://github.com/jemalloc/jemalloc/issues/1274
 
-print travis_template % include_rows
+print(travis_template % include_rows)
---
+++ jemalloc/test/unit/hpa_background_thread.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+export MALLOC_CONF="hpa_dirty_mult:0,background_thread_hpa_interval_max_ms:50,hpa_sec_nshards:0"
