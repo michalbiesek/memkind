3d524b2494e960d8ff711b895bbf78ff87270123
michalbiesek <michal.biesek@intel.com>
2021-06-22 20:22:43 +0200

    pmem_example_modify

--- examples/pmem_cpp_allocator.cpp
+++ examples/pmem_cpp_allocator.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /* Copyright (C) 2018 - 2021 Intel Corporation. */
 
-#include "pmem_allocator.h"
+#include "memkind.h"
 
 #include <cassert>
 #include <iostream>
@@ -11,109 +11,121 @@
 #include <string>
 #include <sys/stat.h>
 #include <vector>
+#include <algorithm>
+#include <random>
+#include <string.h>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <random>
 
-#define STL_VECTOR_TEST
-#define STL_LIST_TEST
-#if _GLIBCXX_USE_CXX11_ABI
-#define STL_VEC_STRING_TEST
-#define STL_MAP_INT_STRING_TEST
-#endif
+#define KB (1024ULL)
+#define MB (1024ULL * (KB))
+#define GB (1024ULL * (MB))
+#define PMEM_SIZE (10 * (GB))
+const size_t SIZE_1 = 4096;
+const size_t SIZE_2 = 64;
+const size_t SIZE_3 = 1024;
+const size_t SIZE_4 = 2048;
 
-void cpp_allocator_test(const char *pmem_directory)
+static struct memkind *pmem_kind;
+static std::vector<void *> pmem_vec;
+auto rng = std::default_random_engine {};
+std::uniform_int_distribution<> distr_1(SIZE_2, SIZE_1);
+std::uniform_int_distribution<> distr_2(SIZE_3, SIZE_4);
+
+void alloc_pattern(size_t size_min, size_t size_max)
 {
-    std::cout << "TEST SCOPE: HELLO" << std::endl;
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+    std::uniform_int_distribution<> distr(size_min, size_max); // define the range
 
-    size_t pmem_max_size = 1024 * 1024 * 1024;
-
-#ifdef STL_VECTOR_TEST
-    {
-        std::cout << "VECTOR OPEN" << std::endl;
-        libmemkind::pmem::allocator<int> alc{pmem_directory, pmem_max_size};
-        std::vector<int, libmemkind::pmem::allocator<int>> vector{alc};
-
-        for (int i = 0; i < 20; ++i) {
-            vector.push_back(0xDEAD + i);
-            assert(vector.back() == 0xDEAD + i);
-        }
-
-        std::cout << "VECTOR CLOSE" << std::endl;
+    size_t total_alloc_size = 0;
+    size_t size;
+    while(1) {
+        size = distr(gen);
+        void *ptr = memkind_malloc(pmem_kind, size);
+        if (!ptr)
+            break;
+        memset(ptr, 'a', size);
+        total_alloc_size += memkind_malloc_usable_size(pmem_kind, ptr);
+        pmem_vec.push_back(ptr);
     }
-#endif
+    std::cout <<  "Vector allocation size min " << size_min << std::endl;
+    std::cout <<  "Vector allocation size max " << size_max << std::endl;
+    std::cout <<  "Total allocated size " << total_alloc_size << std::endl;
+    std::cout <<  "Total allocation no " << pmem_vec.size() << std::endl;
+    std::cout <<  std::endl;
+    //shuffle vector to randomize freeing order
+    std::shuffle(std::begin(pmem_vec), std::end(pmem_vec), rng);
 
-#ifdef STL_LIST_TEST
-    {
-        std::cout << "LIST OPEN" << std::endl;
-        libmemkind::pmem::allocator<int> alc{pmem_directory, pmem_max_size};
-        std::list<int, libmemkind::pmem::allocator<int>> list{alc};
-
-        const int nx2 = 4;
-        for (int i = 0; i < nx2; ++i) {
-            list.emplace_back(0xBEAC011 + i);
-            assert(list.back() == 0xBEAC011 + i);
-        }
-
-        for (int i = 0; i < nx2; ++i) {
-            list.pop_back();
-        }
-
-        std::cout << "LIST CLOSE" << std::endl;
+    //free memory
+    for (auto const &val : pmem_vec) {
+        memkind_free(pmem_kind, val);
     }
-#endif
+    pmem_vec.clear();
+}
 
-#ifdef STL_VEC_STRING_TEST
-    {
-        std::cout << "STRINGED VECTOR OPEN" << std::endl;
-        typedef libmemkind::pmem::allocator<char> str_alloc_t;
-        typedef std::basic_string<char, std::char_traits<char>, str_alloc_t>
-            pmem_string;
-        typedef libmemkind::pmem::allocator<pmem_string> vec_alloc_t;
+void alloc_pattern_2(void)
+{
+    const size_t alloc_no = 1000000000;
+    size_t total_alloc_size = 0;
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
 
-        vec_alloc_t vec_alloc{pmem_directory, pmem_max_size};
-        str_alloc_t str_alloc{pmem_directory, pmem_max_size};
-
-        std::vector<pmem_string, std::scoped_allocator_adaptor<vec_alloc_t>>
-            vec{std::scoped_allocator_adaptor<vec_alloc_t>(vec_alloc)};
-
-        pmem_string arg{"Very very loooong striiiing", str_alloc};
-
-        vec.push_back(arg);
-        assert(vec.back() == arg);
-
-        std::cout << "STRINGED VECTOR CLOSE" << std::endl;
+    for (size_t i = 0; i< alloc_no ;i++) {
+        void *ptr = memkind_malloc(pmem_kind, SIZE_1);
+        if (!ptr)
+            goto status;
+        memset(ptr, 'a', SIZE_1);
+        total_alloc_size += memkind_malloc_usable_size(pmem_kind, ptr);
+        pmem_vec.push_back(ptr);
     }
 
-#endif
-
-#ifdef STL_MAP_INT_STRING_TEST
-    {
-        std::cout << "INT_STRING MAP OPEN" << std::endl;
-        typedef std::basic_string<char, std::char_traits<char>,
-                                  libmemkind::pmem::allocator<char>>
-            pmem_string;
-        typedef int key_t;
-        typedef pmem_string value_t;
-        typedef libmemkind::pmem::allocator<std::pair<const key_t, value_t>>
-            allocator_t;
-        typedef std::map<key_t, value_t, std::less<key_t>,
-                         std::scoped_allocator_adaptor<allocator_t>>
-            map_t;
-
-        allocator_t allocator(pmem_directory, pmem_max_size);
-
-        value_t source_str1("Lorem ipsum dolor ", allocator);
-        value_t source_str2("sit amet consectetuer adipiscing elit", allocator);
-
-        map_t target_map{std::scoped_allocator_adaptor<allocator_t>(allocator)};
-
-        target_map[key_t(165)] = source_str1;
-        assert(target_map[key_t(165)] == source_str1);
-        target_map[key_t(165)] = source_str2;
-        assert(target_map[key_t(165)] == source_str2);
-
-        std::cout << "INT_STRING MAP CLOSE" << std::endl;
+    for (size_t i = 0; i< alloc_no ;i++) {
+        void *ptr = memkind_malloc(pmem_kind, SIZE_2);
+        if (!ptr)
+            goto status;
+        memset(ptr, 'a', SIZE_2);
+        total_alloc_size += memkind_malloc_usable_size(pmem_kind, ptr);
+        pmem_vec.push_back(ptr);
     }
-#endif
-    std::cout << "TEST SCOPE: GOODBYE" << std::endl;
+
+    for (size_t i = 0; i< alloc_no ;i++) {
+        size_t size = distr_1(gen);
+        void *ptr = memkind_malloc(pmem_kind, size);
+        if (!ptr)
+            goto status;
+        memset(ptr, 'a', size);
+        total_alloc_size += memkind_malloc_usable_size(pmem_kind, ptr);
+        pmem_vec.push_back(ptr);
+    }
+
+    for (size_t i = 0; i< alloc_no ;i++) {
+        size_t size = distr_2(gen);
+        void *ptr = memkind_malloc(pmem_kind, size);
+        if (!ptr)
+            goto status;
+        memset(ptr, 'a', size);
+        total_alloc_size += memkind_malloc_usable_size(pmem_kind, ptr);
+        pmem_vec.push_back(ptr);
+    }
+
+
+status:
+    std::cout <<  "Total allocated size " << total_alloc_size << std::endl;
+    std::cout <<  "Total allocation no " << pmem_vec.size() << std::endl;
+    std::cout <<  std::endl;
+
+    //shuffle vector to randomize freeing order
+    std::shuffle(std::begin(pmem_vec), std::end(pmem_vec), rng);
+
+    //free memory
+    for (auto const &val : pmem_vec) {
+        memkind_free(pmem_kind, val);
+    }
+    pmem_vec.clear();
 }
 
 int main(int argc, char *argv[])
@@ -122,28 +134,42 @@ int main(int argc, char *argv[])
 
     if (argc > 2) {
         std::cerr
-            << "Usage: pmem_cpp_allocator [directory path]\n"
+            << "Usage: test_app [directory path]\n"
             << "\t[directory path] - directory where temporary file is created (default = \"/tmp/\")"
             << std::endl;
         return 0;
     } else if (argc == 2) {
-        struct stat st;
-        if (stat(argv[1], &st) != 0 || !S_ISDIR(st.st_mode)) {
-            fprintf(stderr, "%s : Invalid path to pmem kind directory\n",
-                    argv[1]);
-            return 1;
-        }
         int status = memkind_check_dax_path(argv[1]);
         if (!status) {
-            std::cout << "PMEM kind %s is on DAX-enabled File system.\n"
-                      << std::endl;
+            std::cout << "PMEM kind is on DAX-enabled File system.\n"<< argv[1]<< std::endl;
         } else {
-            std::cout << "PMEM kind %s is not on DAX-enabled File system.\n"
-                      << std::endl;
+            std::cout << "PMEM kind is not on DAX-enabled File system.\n"<< argv[1]<< std::endl;
         }
         pmem_directory = argv[1];
     }
 
-    cpp_allocator_test(pmem_directory);
+    //reserve vector to avoid resize it
+    const size_t alloc = 150000000;
+    pmem_vec.reserve(alloc);
+
+    std::cout <<  "Size of PMEM kind: " << PMEM_SIZE << std::endl;
+
+    int ret = memkind_create_pmem(pmem_directory, (size_t)(PMEM_SIZE), &pmem_kind);
+
+    if (ret)
+        return -1;
+
+    for (int i = 0; i <10 ; i++)
+    {
+        std::cout <<  "Iteration no " << i << std::endl;
+        // alloc_pattern(SIZE_1, SIZE_1);
+        // alloc_pattern(SIZE_2, SIZE_2);
+        // alloc_pattern(SIZE_2, SIZE_1);
+        // alloc_pattern(SIZE_3, SIZE_4);
+        alloc_pattern_2();
+    }
+
+    ret = memkind_destroy_kind(pmem_kind);
+
     return 0;
 }
